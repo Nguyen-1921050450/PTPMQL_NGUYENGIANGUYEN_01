ffc20b0070edd20048af7c48304ef83c46f34556
nguyen <kaitokuroba507@gmail.com>
2023-08-04 10:18:01 +0700

    NguyenGiaNguyen_Cau2

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace NguyenGiaNguyen_01.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
